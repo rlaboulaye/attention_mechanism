fb0983394b11b20bb06fdf4fca6030de2e007590
Roland Laboulaye <rlaboulaye@gmail.com>
2018-03-27 01:18:00 -0600

    corrected dimensions of stacked GRUs

--- RNN.py
+++ RNN.py
@@ -5,13 +5,18 @@ from GRUCell import GRUCell
 class RNN(nn.Module):
 
 	def __init__(self, input_dimension, hidden_dimension, num_layers):
+		super(RNN, self).__init__()
 		if num_layers < 1:
 			raise ValueError('num_layers must be 1 or greater')
 		self.input_dimension = input_dimension
 		self.hidden_dimension = hidden_dimension
 		self.layers = []
 		for i in range(num_layers):
-			self.layers.append(GRUCell(self.input_dimension, self.hidden_dimension))
+			if i == 0:
+				input_dimension = self.input_dimension
+			else:
+				input_dimension = self.hidden_dimension
+			self.layers.append(GRUCell(input_dimension, self.hidden_dimension))
 
 	def forward(self, x_t, h_tm1):
 		h_t = []

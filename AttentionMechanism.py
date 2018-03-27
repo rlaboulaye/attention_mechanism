69849b0bc03c117d8b1940deb41b070f14ec948c
Roland Laboulaye <rlaboulaye@gmail.com>
2018-03-27 02:31:58 -0600

    AttentionMechanism runs

--- AttentionMechanism.py
+++ AttentionMechanism.py
@@ -1,10 +1,14 @@
+import numpy as np
 import torch
 from torch import nn
 from torch.autograd import Variable
 
 class AttentionMechanism(nn.Module):
 
+	# TODO: add initialization for fc; cuda
+
 	def __init__(self, state_dimension, embedding_dimension):
+		super(AttentionMechanism, self).__init__()
 		self.fc = nn.Linear(state_dimension + embedding_dimension, 1)
 		self.probability = nn.Softmax()
 
@@ -13,5 +17,12 @@ class AttentionMechanism(nn.Module):
 		for embedding in embeddings:
 			concatenated_embedding = torch.cat((state_tm1, embedding), dim=1)
 			unnorm_alphas.append(self.fc(concatenated_embedding))
-		alphas = self.probability(Variable(torch.FloatTensor(unnorm_alphas)))
-		return alphas * embeddings
+		alphas = self.probability(torch.cat([torch.unsqueeze(alpha, dim=0) for alpha in unnorm_alphas], dim=0))
+		weighted_combination = None
+		for i in range(len(embeddings)):
+			weighted_embedding = alphas[i] * embeddings[i]
+			if weighted_combination is None:
+				weighted_combination = weighted_embedding
+			else:
+				weighted_combination += weighted_embedding
+		return weighted_combination

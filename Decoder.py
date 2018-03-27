f89784c663a1d4ebf37aeb7f33d776a9edb60084
Roland Laboulaye <rlaboulaye@gmail.com>
2018-03-27 01:18:17 -0600

    initial Decoder

---
+++ Decoder.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+from RNN import RNN
+
+class Decoder(nn.Module):
+
+	def __init__(self, input_dimension=512, hidden_dimension=1024, output_dimension=5000, num_layers=1, batch_size=1):
+		super(Decoder, self).__init__()
+		self.input_dimension = input_dimension
+		self.hidden_dimension = hidden_dimension
+		self.output_dimension = output_dimension
+		self.num_layers = num_layers
+		self.batch_size = batch_size
+		self.rnn = RNN(self.input_dimension, self.hidden_dimension, self.num_layers)
+		self.fc = nn.Linear(self.hidden_dimension, self.output_dimension)
+		self.output_activation = nn.Softmax()
+		self.x_0 = Variable(torch.FloatTensor(np.zeros((self.batch_size, self.hidden_dimension))))
+		if torch.cuda.is_available():
+			self.x_0.cuda()
+
+	def _get_output(self, hidden_state, generate):
+		logits = self.fc(hidden_state)
+		if generate:
+			return self.output_activation(logits)
+		else:
+			return logits
+
+	def forward(self, sequence_embedding, generate=False):
+		sequence = []
+		x = self.x_0
+		h_tm1 = sequence_embedding
+		for input_embedding in input_sequence:
+			h_tm1 = self.forward_rnn(x, h_tm1)
+			x = self._get_output(h_tm1[-1], generate)
+			sequence.append(x)
+		return sequence

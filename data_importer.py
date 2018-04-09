e06153f1e162c27943577b6afa1959eb13a78376
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2018-04-09 17:18:19 -0600

    correctly read and parse text

--- data_importer.py
+++ data_importer.py
@@ -15,17 +15,50 @@ from torch.utils.data import Dataset
 
 class SentenceTranslationDataset(Dataset):
 
-    UNKNOWN = "UNKNOWN"
-    EOS = "EOS"
+    UNKNOWN_TOKEN = "<unk>"
+    EOS_TOKEN = "</s>"
 
+    # # default
+    # def __init__(
+    #     self,
+    #     src_lang_vocab_path="./data/en-de/vocab.50K.en",
+    #     targ_lang_vocab_path="./data/en-de/vocab.50K.de",
+    #     src_lang_embedding_path="./data/fastText/wiki.en.vec",
+    #     targ_lang_embedding_path="./data/fastText/wiki.de.vec",
+    #     src_lang_path="./data/en-de/train.en",
+    #     targ_lang_path="./data/en-de/train.de",
+    #     max_vocab_size=None,
+    #     max_n_sentences=None,
+    #     max_src_sentence_len=None,
+    #     prune_by_vocab=False,
+    #     prune_by_embedding=False
+    # ):
+
+    # # embedding as vocab
+    # def __init__(
+    #     self,
+    #     src_lang_vocab_path="./data/fastText/wiki.en.vec.vocab",
+    #     targ_lang_vocab_path="./data/fastText/wiki.de.vec.vocab",
+    #     src_lang_embedding_path="./data/fastText/wiki.en.vec",
+    #     targ_lang_embedding_path="./data/fastText/wiki.de.vec",
+    #     src_lang_path="./data/en-de/train.en",
+    #     targ_lang_path="./data/en-de/train.de",
+    #     max_vocab_size=None,
+    #     max_n_sentences=None,
+    #     max_src_sentence_len=None,
+    #     prune_by_vocab=False,
+    #     prune_by_embedding=False
+    # ):
+
+    # english to english
     def __init__(
         self,
-        src_lang_vocab_path="./data/en-de/vocab.50K.en",
-        targ_lang_vocab_path="./data/en-de/vocab.50K.de",
+        src_lang_vocab_path="./data/fastText/wiki.en.vec.vocab",
+        targ_lang_vocab_path="./data/fastText/wiki.en.vec.vocab",
         src_lang_embedding_path="./data/fastText/wiki.en.vec",
-        targ_lang_embedding_path="./data/fastText/wiki.de.vec",
+        targ_lang_embedding_path="./data/fastText/wiki.en.vec",
         src_lang_path="./data/en-de/train.en",
-        targ_lang_path="./data/en-de/train.de",
+        targ_lang_path="./data/en-de/train.en",
         max_vocab_size=None,
         max_n_sentences=None,
         max_src_sentence_len=None,
@@ -66,40 +99,40 @@ class SentenceTranslationDataset(Dataset):
         self._init_text()
         self._init_batching()
 
+    def _open_file(self, path, mode, encoding="utf-8"):
+        return io.open(path, mode, encoding=encoding)
+
+    def _parse_word(self, word):
+        word = word.strip().lower()
+        word = self.html_parser.unescape(word)
+        return word
+
     def _init_vocab(self):
-        '''
-        line 0 - unknown token
-        line 2 - end of sentence token
-        '''
-        # todo consider deleting src_vocab, it is not used anywhere else
-        self.src_vocab, self.src_word_2_encoding = self._read_vocab(self.src_lang_vocab_path, self.max_vocab_size)
-        self.src_vocab_set = set(self.src_vocab)
-        self.targ_vocab, self.targ_word_2_encoding = self._read_vocab(self.targ_lang_vocab_path, self.max_vocab_size)
-        self.targ_vocab_set = set(self.targ_vocab)
+        self.src_vocab, self.src_vocab_2_encoding = self._read_vocab(self.src_lang_vocab_path)
+        self.targ_vocab, self.targ_vocab_2_encoding = self._read_vocab(self.targ_lang_vocab_path)
 
-        self.special_tokens = {
-            self.UNKNOWN: self.src_vocab[0],
-            self.EOS: self.src_vocab[2]
-        }
-
-    def _init_targ_encoding(self):
-        self.targ_word_2_encoding = {}
-        targ_word_encoding_index = 0
-        for word in self.targ_vocab_set:
-            self.targ_word_2_encoding[word] = targ_word_encoding_index
-            targ_word_encoding_index += 1
-
-    def _read_vocab(self, path, max_vocab_size):
+    def _read_vocab(self, path):
         vocab = []
         word_2_encoding = {}
-        with io.open(path, "r", encoding="utf-8") as f:
-        # with open(path, "r") as f:
+        with self._open_file(path, "r") as f:
             for i, line in enumerate(f):
-                word = line.strip().lower() # one word per line
+                line = line.strip()
+                if line == "" or (self.max_vocab_size is not None and i >= self.max_vocab_size):
+                        break
+                word = self._parse_word(line) # one word per line
                 vocab.append(word)
                 word_2_encoding[word] = i
-                if max_vocab_size is not None and i >= max_vocab_size:
-                    break
+
+        if self.UNKNOWN_TOKEN not in word_2_encoding:
+            vocab.append(self.UNKNOWN_TOKEN)
+            word_2_encoding[self.UNKNOWN_TOKEN] = i
+            i += 1
+
+        if self.EOS_TOKEN not in word_2_encoding:
+            vocab.append(self.EOS_TOKEN)
+            word_2_encoding[self.EOS_TOKEN] = i
+            i += 1
+
         return vocab, word_2_encoding
 
     def _init_embedding(self):
@@ -107,14 +140,14 @@ class SentenceTranslationDataset(Dataset):
         if os.path.isfile(cache_src_emb_path):
             self.src_word_2_embedding = pickle.load(open(cache_src_emb_path, "rb"))
         else:
-            self.src_word_2_embedding = self._read_embedding(self.src_lang_embedding_path)
+            self.src_word_2_embedding = self._read_embedding(self.src_lang_embedding_path, self.src_vocab_2_encoding)
             pickle.dump(self.src_word_2_embedding, open(cache_src_emb_path, "wb"))
 
         cache_targ_emb_path = self._get_cache_targ_emb_path()
         if os.path.isfile(cache_targ_emb_path):
             self.targ_word_2_embedding = pickle.load(open(cache_targ_emb_path, "rb"))
         else:
-            self.targ_word_2_embedding = self._read_embedding(self.targ_lang_embedding_path)
+            self.targ_word_2_embedding = self._read_embedding(self.targ_lang_embedding_path, self.targ_vocab_2_encoding)
             pickle.dump(self.targ_word_2_embedding, open(cache_targ_emb_path, "wb"))
 
     def _get_cache_src_emb_path(self):
@@ -125,27 +158,25 @@ class SentenceTranslationDataset(Dataset):
 
     def _get_cache_targ_emb_path(self):
         hash_str = str(abs(hash(
-            self.targ_lang_embedding_path + self.src_lang_vocab_path + str(self.max_vocab_size)
+            self.targ_lang_embedding_path + self.targ_lang_vocab_path + str(self.max_vocab_size)
         )))
         return self.cache_dir + "targ_emb_" + hash_str + ".p"
 
-    def _read_embedding(self, path):
+    def _read_embedding(self, path, vocab_lookup):
         word_2_embedding = {}
-        with open(path, "r") as f:
+        with self._open_file(path, "r") as f:
             for i, line in enumerate(f):
                 if i != 0 and line != "":
-                    # print line
-                    # if i == 50:
-                    #     exit(1)
                     word, str_embedding = line.strip().split(" ", 1)
-                    word = word.lower()
-                    if word in self.src_vocab_set:
+                    word = self._parse_word(word)
+                    if word in vocab_lookup:
                         word_2_embedding[word] = np.array(str_embedding.split(" "), dtype=float)
-                    # else:
-                    #     print word
-
+                if len(word_2_embedding) == len(vocab_lookup): # will not get any more words, no matter how much we search
+                    break
         embedding_size = word_2_embedding.itervalues().next().shape[0]
-        word_2_embedding[self.special_tokens[self.UNKNOWN]] = np.zeros(embedding_size)
+        word_2_embedding[self.UNKNOWN_TOKEN] = np.zeros(embedding_size)
+        if self.EOS_TOKEN not in word_2_embedding:
+            raise ValueError(path + " does not contain an end of sequence embedding")
         return word_2_embedding
 
     def _init_text(self):
@@ -160,19 +191,18 @@ class SentenceTranslationDataset(Dataset):
         self.src_data = []
         self.targ_data = []
         self.src_data_by_seq_len_indices = []
-        with io.open(self.src_lang_path, "r", encoding='utf8') as f_src, io.open(self.targ_lang_path, "r", encoding='utf8') as f_targ:
-        # with open(self.src_lang_path, "r") as f_src, open(self.targ_lang_path, "r") as f_targ:
+        with self._open_file(self.src_lang_path, "r") as f_src, self._open_file(self.targ_lang_path, "r") as f_targ:
             for i, (src_line, targ_line) in enumerate(izip(f_src, f_targ)):
-                src_sentence = self._parse_line(src_line, "src")
-                targ_sentence = self._parse_line(targ_line, "targ")
+                src_sentence = self._parse_text_line(src_line, "src")
+                targ_sentence = self._parse_text_line(targ_line, "targ")
 
                 if None in [src_sentence, targ_sentence]: # it was pruned
                     self.pruned_sentence_count += 1
-                    # print i
                 else:
                     self.src_data.append(src_sentence)
                     self.targ_data.append(targ_sentence)
 
+                    # store indices to src data by sequence length
                     while len(self.src_data_by_seq_len_indices) <= len(src_sentence):
                         self.src_data_by_seq_len_indices.append([])
                     self.src_data_by_seq_len_indices[len(src_sentence)].append(len(self.src_data)-1)
@@ -180,35 +210,29 @@ class SentenceTranslationDataset(Dataset):
                 if self.max_n_sentences is not None and len(self.src_data) >= self.max_n_sentences:
                     break
 
-    def _parse_line(self, line, line_type):
+    def _parse_text_line(self, line, line_type):
         if line_type == "src":
-            vocab_set = self.src_vocab_set
+            vocab_lookup = self.src_vocab_2_encoding
             word_2_embedding = self.src_word_2_embedding
         elif line_type == "targ":
-            vocab_set = self.targ_vocab_set
+            vocab_lookup = self.targ_vocab_2_encoding
             word_2_embedding = self.targ_word_2_embedding
-            # line = line.replace(u"\xfc", u"\xc3\xbc")# "ü")
-            # "Ä","ä","Ö","ö","Ü",,"ß"
         else:
             raise ValueError("invalid line_type")
-
-        line = line.lower()
-        line = self.html_parser.unescape(line)
 
         unknown_vocab_count = 0
         unknown_embedding_count = 0
         sentence = []
         for word in line.strip().split(" "):
-            if word not in vocab_set:
+            word = self._parse_word(word)
+            if word not in vocab_lookup:
                 unknown_vocab_count += 1
-                word = self.special_tokens[self.UNKNOWN]
+                word = self.UNKNOWN_TOKEN
             elif word not in word_2_embedding:
-                # if line_type == "targ":
-                #     print word
                 unknown_embedding_count += 1
-                word = self.special_tokens[self.UNKNOWN]
+                word = self.UNKNOWN_TOKEN
             sentence.append(word)
-        sentence.append(self.special_tokens[self.EOS])
+        sentence.append(self.EOS_TOKEN)
 
         if line_type == "src":
             self.known_src_word_count += len(sentence) - unknown_vocab_count - unknown_embedding_count
@@ -227,9 +251,9 @@ class SentenceTranslationDataset(Dataset):
             return sentence
 
     def _init_batching(self):
-        self.batch_start_indices = [0 for _ in xrange(len(self.src_data_by_seq_len_indices))]
+        self.batch_start_indices = [0] * len(self.src_data_by_seq_len_indices)
 
-    def batch(self, src_seq_len, batch_size):
+    def batch(self, src_seq_len, batch_size, shuffle=True):
         if src_seq_len >= len(self.batch_start_indices):
             raise ValueError("src_seq_len is too long")
 
@@ -241,7 +265,8 @@ class SentenceTranslationDataset(Dataset):
             self.batch_start_indices[src_seq_len] = batch_end_index
 
         data_indices = self.src_data_by_seq_len_indices[src_seq_len][batch_start_index:batch_end_index]
-
+        print self.src_data[data_indices[0]]
+        print self.targ_data[data_indices[0]]
         batch_src_data = np.array([self._embed_src_sentence(self.src_data[i]) for i in data_indices])
         batch_src_data = np.swapaxes(batch_src_data, 0, 1)
 
@@ -257,7 +282,7 @@ class SentenceTranslationDataset(Dataset):
         for targ_data in jagged_batch_targ_data:
             pad_width = (0, max_targ_seq_len - len(targ_data))
             if pad_width != (0,0):
-                targ_data = np.pad(targ_data, pad_width, mode="constant", constant_values=self.targ_word_2_encoding[self.special_tokens[self.EOS]])
+                targ_data = np.pad(targ_data, pad_width, mode="constant", constant_values=self.targ_vocab_2_encoding[self.EOS_TOKEN])
             batch_targ_data.append(targ_data)
 
         batch_targ_data = np.array(batch_targ_data)
@@ -269,9 +294,9 @@ class SentenceTranslationDataset(Dataset):
         return np.array([self.src_word_2_embedding[word] for word in sentence])
 
     def _encode_targ_sentence(self, sentence):
-        return np.array([self.targ_word_2_encoding[word] for word in sentence])
+        return np.array([self.targ_vocab_2_encoding[word] for word in sentence])
 
-dataset = SentenceTranslationDataset(max_n_sentences=1e5, max_vocab_size=None, max_src_sentence_len=40)#, prune_by_embedding=True)
+dataset = SentenceTranslationDataset(max_n_sentences=1e6, max_vocab_size=1e4, max_src_sentence_len=40, prune_by_vocab=True, prune_by_embedding=True)#, prune_by_embedding=True)
 print dataset.unknown_src_vocab_count / dataset.known_src_word_count, dataset.unknown_src_embedding_count / dataset.known_src_word_count
 print dataset.unknown_targ_vocab_count / dataset.known_targ_word_count, dataset.unknown_targ_embedding_count / dataset.known_targ_word_count
 print dataset.pruned_sentence_count
@@ -280,8 +305,8 @@ print len(dataset.src_data), len(dataset.targ_data)
 # for i, item in enumerate(dataset.src_data_by_seq_len_indices):
 #     print i, len(item)
 
-for i in xrange(10):
-    dataset.batch(10, 25)[:2]
+for i in xrange(7):
+    dataset.batch(13, 17)
 
 
 # emb_path = "./data/fastText/wiki.de.vec"
@@ -292,7 +317,7 @@ for i in xrange(10):
 #             word, str_embedding = line.strip().split(" ", 1)
 #             word = word.lower()
 #             word_2_embedding[word] = np.array(str_embedding.split(" "), dtype=float)
-#         if i == 100: 
+#         if i == 100:
 #             break
 # print word_2_embedding.keys()
 

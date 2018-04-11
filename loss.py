90dfa27f137f165f264d0afb1ebeaf59cea6703a
Roland Laboulaye <rlaboulaye@gmail.com>
2018-04-11 01:18:08 -0600

    changed filtering mechanism

--- loss.py
+++ loss.py
@@ -6,11 +6,13 @@ class SequenceLoss(nn.Module):
 
 	def __init__(self, element_loss=CrossEntropyLoss):
 		super(SequenceLoss, self).__init__()
-		self.element_loss = element_loss()
+		self.element_loss = element_loss(reduce=False)
 
 	def forward(self, sequence_of_logits, sequence_of_targets):
 		losses = []
 		for logits, targets in zip(sequence_of_logits, sequence_of_targets):
-			indices = targets != None
-			losses.append(self.element_loss(logits[indices], targets[indices]))
-		return np.mean([loss for batch_loss in losses for loss in batch_loss])
+			mask = targets != -1
+			batch_loss = self.element_loss(logits, targets)
+			masked_batch_loss = mask.float() * batch_loss
+			losses.append(masked_batch_loss.mean())
+		return np.mean(losses)
